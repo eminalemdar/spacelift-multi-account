1c43ac09b01a0151ad8869994779e34611af90d8
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-10-17 10:35:31 +0300

    Updated S3 config

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "demo" {
+resource "aws_s3_bucket" "demo1" {
   bucket = "demo-bucket-emnalmdr1"
   provider = aws
 
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "demo" {
   }
 }
 
-resource "aws_s3_bucket" "demo" {
+resource "aws_s3_bucket" "demo2" {
   bucket = "demo-bucket-emnalmdr2"
   provider = aws.second-account
 
