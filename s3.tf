85c0a05d9594e20de493d8ec78014393971e41aa
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-10-25 16:10:25 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,15 +1,5 @@
-resource "aws_s3_bucket" "demo1" {
-  bucket   = "demo-bucket-emnalmdr1"
-  provider = aws
-
-  tags = {
-    Name = "demo-bucket-emnalmdr1"
-  }
-}
-
 resource "aws_s3_bucket" "demo2" {
   bucket   = "demo-bucket-emnalmdr2"
-  provider = aws.second-account
 
   tags = {
     Name = "demo-bucket-emnalmdr2"
